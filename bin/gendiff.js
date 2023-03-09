34662e72976b81dcb22452cb2a3a98435f295e0f
Максим Григорьев <grigorev.maksim.1999@gmail.com>
2023-03-09 20:39:39 +0700

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1');
+  .version('0.1')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse();
