961ae5928d8eeb4656e681195d4ea8a9240ca6bb
Максим Григорьев <grigorev.maksim.1999@gmail.com>
2023-03-16 17:34:14 +0700

    step 3

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,28 +3,32 @@ import _ from 'lodash';
 const genDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const keys = _.union(keys1, keys2)
+  const keys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(keys)
     .reduce((acc, key) => {
       if (!Object.hasOwn(obj1, key)) {
-        acc[`+ ${key}`] = obj2[key];
+        // acc[`+ ${key}`] = obj2[key];
+        acc.push(`  + ${key}: ${obj2[key]}`);
         return acc;
       }
       if (!Object.hasOwn(obj2, key)) {
-        acc[`- ${key}`] = obj1[key];
+        // acc[`- ${key}`] = obj1[key];
+        acc.push(`  - ${key}: ${obj1[key]}`);
         return acc;
       }
       if (obj1[key] !== obj2[key]) {
-        acc[`- ${key}`] = obj1[key];
-        acc[`+ ${key}`] = obj2[key];
+        acc.push(`  - ${key}: ${obj1[key]}`);
+        acc.push(`  + ${key}: ${obj2[key]}`);
         return acc;
       }
       if (obj1[key] === obj2[key]) {
-        acc[`  ${key}`] = obj1[key];
+        acc.push(`    ${key}: ${obj1[key]}`);
         return acc;
       }
       return acc;
-    }, {});
-  return keys;
+    }, []);
+  const str = sortedKeys.join('\n');
+  return `{\n${str}\n}`;
 };
 
 export default genDiff;
